5c3d1f4bae9344a188e0384481eca2f8f37144b2
Benjamin Staneck <staneck@gmail.com>
2019-04-14 18:48:58 +0200

    Initial Commit

---
+++ DBM-SMGEventsPack/LoadList.lua
@@ -0,0 +1,60 @@
+local inserted1, inserted2, inserted3 = false, false, false
+--Try to use unique function names so diff user packs don't overwrite eachother, Generally I just use MP, VP, DF + Unique Addon Name
+--Function names are defined in TOC. A pack does not need to contain all 3 sound types. You are welcome to make just specific ones, just make sure TOC is correct
+function DBMVPSMGPack()--Register Victory sounds to DBM.Victory table
+	if inserted then return end
+	tinsert(DBM.Victory, {text = "SMG: FF Fanfare (Long)",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Victory\\SmoothMcGroove_Fanfare_Long.ogg"})
+	inserted1 = true
+end
+--[[
+function DBMDPSMGPack()--Register Defeat sounds to DBM.Defeat Table
+	if inserted then return end
+	--tinsert(DBM.Defeat, {text = "Alex Death",	value = "Interface\\AddOns\\DBM-SmoothMcGrooveEventsPack\\Defeat\\alexdeath.ogg"})
+	inserted2 = true
+end
+--]]
+function DBMMPSMGPack()--Register Music to DBM.Music Table
+	if inserted then return end
+	tinsert(DBM.Music, {text = "SMG: CT Corridors of Time",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\ChronoTrigger_CorridorsofTime.ogg"})
+	tinsert(DBM.Music, {text = "SMG: FFVI Decisive Battle",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\FF6_DecisiveBattleAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: FFVII Battle",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\FF7_BattleThemeAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: FFVII JENOVA",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\FF7_JENOVAAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: FFVII One Winged Angel",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\FF7_OneWingedAngelAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: FFX Battle",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\FFX_BattleAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: MM2 Dr. Wily Stage",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\MM2_DrWilyStageAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: MMX Spark Mandrill Stage",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\MMX_SparkMandrillAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: MMX Stage Select",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\MMX_StageSelectAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: Super Mario Kart Mario Ciruit",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\SMK_MarioCircuitAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: Secret of Mana Ice Palace",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\SoM_ABellisTollingAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: Super Metroid Brinstar Green",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\SuperMetroid_BrinstarGreen.ogg"})
+	tinsert(DBM.Music, {text = "SMG: Tetris Theme A",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\Tetris_ThemeAAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: WoW Pirate Tavern",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\WoW_PirateTavernAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: Xenogears One Who Bares...",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\Xenogears_OneWhoBaresFangs.ogg"})
+	tinsert(DBM.Music, {text = "SMG: Zelda Dungeon",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\Zelda_DungeonAcapella.ogg"})
+	tinsert(DBM.Music, {text = "SMG: Zelda 2 Temple Palace",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\Zelda2_TemplePalaceAcapella.ogg"})
+	
+	--Dungeon BGM Table
+	if DBM.DungeonMusic then
+		tinsert(DBM.DungeonMusic, {text = "SMG: CT Corridors of Time",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\ChronoTrigger_CorridorsofTime.ogg"})
+		tinsert(DBM.DungeonMusic, {text = "SMG: MM2 Dr. Wily Stage",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\MM2_DrWilyStageAcapella.ogg"})
+		tinsert(DBM.DungeonMusic, {text = "SMG: MMX Spark Mandrill Stage",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\MMX_SparkMandrillAcapella.ogg"})
+		tinsert(DBM.DungeonMusic, {text = "SMG: MMX Stage Select",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\MMX_StageSelectAcapella.ogg"})
+		tinsert(DBM.DungeonMusic, {text = "SMG: Super Mario Kart Mario Ciruit",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\SMK_MarioCircuitAcapella.ogg"})
+		tinsert(DBM.DungeonMusic, {text = "SMG: Secret of Mana Ice Palace",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\SoM_ABellisTollingAcapella.ogg"})
+		tinsert(DBM.DungeonMusic, {text = "SMG: Super Metroid Brinstar Green",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\SuperMetroid_BrinstarGreen.ogg"})
+		tinsert(DBM.DungeonMusic, {text = "SMG: WoW Pirate Tavern",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\WoW_PirateTavernAcapella.ogg"})
+		tinsert(DBM.DungeonMusic, {text = "SMG: Zelda Dungeon",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\Zelda_DungeonAcapella.ogg"})
+		tinsert(DBM.DungeonMusic, {text = "SMG: Zelda 2 Temple Palace",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\Zelda2_TemplePalaceAcapella.ogg"})
+	end
+	--Boss BGM Table
+	if DBM.BattleMusic then
+		tinsert(DBM.BattleMusic, {text = "SMG: FFVI Decisive Battle",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\FF6_DecisiveBattleAcapella.ogg"})
+		tinsert(DBM.BattleMusic, {text = "SMG: FFVII Battle",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\FF7_BattleThemeAcapella.ogg"})
+		tinsert(DBM.BattleMusic, {text = "SMG: FFVII JENOVA",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\FF7_JENOVAAcapella.ogg"})
+		tinsert(DBM.BattleMusic, {text = "SMG: FFVII One Winged Angel",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\FF7_OneWingedAngelAcapella.ogg"})
+		tinsert(DBM.BattleMusic, {text = "SMG: FFX Battle",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\FFX_BattleAcapella.ogg"})
+		tinsert(DBM.BattleMusic, {text = "SMG: Tetris Theme A",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\Tetris_ThemeAAcapella.ogg"})
+		tinsert(DBM.BattleMusic, {text = "SMG: Xenogears One Who Bares...",	value = "Interface\\AddOns\\DBM-SMGEventsPack\\Music\\Xenogears_OneWhoBaresFangs.ogg"})
+	end
+	inserted3 = true
+end
